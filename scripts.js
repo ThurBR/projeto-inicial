8e97c3ab727498a33fae70042e72f94c0eac4b90
ThuBR <arthursilvamartins07@yahoo.com>
2024-03-19 09:15:53 -0300

    Formulário

--- scripts.js
+++ scripts.js
@@ -23,15 +23,34 @@ var cidade = document.getElementById("cidade");
 var estado = document.getElementById("estado");
 
 function alertar(){
+
+//buscar o endereço pelo cep
+const url = `https://viacep.com.br/ws/${cep.value}/json`;
+
+fetch(url)
+.then(resposta=>resposta.json())
+.then(data => {
+  logradouro.value = data.logradouro;
+  bairro.value = data.bairro;
+  cidade.value = data.cidade;
+  estado.value = data.uf;
+
+
+
+
+
     //alert(nome.value + " " + "clicou no botão!!!");
     saida.innerText = "Nome: " + " " + nome.value + 
                 "\nE-mail: " + " " + email.value +
                 "\nTelefone: " + " " + telefone.value +
-                "\nCPF: " + " " + cpf.value +
+                "\nCEP: " + " " + cep.value +
                 "\nLogradouro: " + " " + logradouro.value +
                 "\nNumero: " + " " + numero.value +
                 "\nComplemento: " + " " + complemento.value +
                 "\nBairro: " + " " + bairro.value +
                 "\nCidade: " + " " + cidade.value +
                 "\nEstado: " + " " + estado.value;
+
+})
+.catch(error=>alert(error))
 }
